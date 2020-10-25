aaa20418b1e7ada902111164b7c15a327aa1d481
Raul Cocis <raulsergiuvasile.cocis@e-campus.uab.cat>
2020-10-25 22:04:08 +0100

    25% Front-end

---
+++ technicaltest_front/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class= "container">
+    
+    
+    <General></General>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import General from './components/General.vue';
+
+@Component({
+  components: {
+    General
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ technicaltest_front/src/components/General.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img alt="team cmp logo" width="25%" height="25%" src="@/assets/teamcmp-logo.png" class="rounded float-right" >
+        <h1>Technical Test</h1>
+
+        <div class="mt-3">
+            <div class= "alert alert-danger" role="alert">
+                Text Lorem ipsum
+            </div>
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    name: 'General'
+}
+</script>
